905a54a3939b3efbf6579f14e966cd980b6b71f0
Salima Ammar <selimaammar07@gmail.com>
2025-05-17 19:21:46 -0400

    Add echo command functionnality

--- src/main.c
+++ src/main.c
@@ -1,18 +1,48 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(){
-    setbuf(stdout, NULL); 
-    while(1){
+int main()
+{
+    setbuf(stdout, NULL);
+    while (1)
+    {
         printf("$ ");
         char input[100];
-        
-        if(fgets(input, 100, stdin) == NULL){
+
+        if (fgets(input, 100, stdin) == NULL)
+        {
             break;
         }
-        
-        input[strlen(input) - 1] = '\0'; 
-        printf("%s: command not found\n", input);
-    } 
+
+        input[strlen(input) - 1] = '\0';
+
+        char *ptr = strtok(input, " ");
+
+        while (ptr != NULL)
+        {
+            // exit command
+            if (strcmp(ptr, "exit") == 0)
+            {
+                return 0;
+            }
+
+            // echo command
+            else if (strcmp(ptr, "echo") == 0)
+            {
+                ptr = strtok(NULL, " ");
+                while (ptr != NULL)
+                {
+                    printf("%s", ptr);
+                    ptr = strtok(NULL, " ");
+                }
+                printf("\n");
+            }
+            else
+            {
+                printf("%s: command not found\n", input);
+                break;
+            }
+        }
+    }
     return 0;
 }
