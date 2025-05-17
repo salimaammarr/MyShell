03d2002d80a88d1152368a83b95849bc835902ad
Salima Ammar <selimaammar07@gmail.com>
2025-05-17 18:44:03 -0400

    Maintain basic shell loop with command not found response

--- src/main.c
+++ src/main.c
@@ -3,12 +3,16 @@
 
 int main(){
     setbuf(stdout, NULL); 
-    printf('$');
-    
-    char input[100];
-    fgets(input,100,stdin);
-    
-    input[strlen(input)-1] = '\0';
-    printf("%s: command not found\n", input);
+    while(1){
+        printf("$ ");
+        char input[100];
+        
+        if(fgets(input, 100, stdin) == NULL){
+            break;
+        }
+        
+        input[strlen(input) - 1] = '\0'; 
+        printf("%s: command not found\n", input);
+    } 
     return 0;
 }
